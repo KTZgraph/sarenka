e73737e7a327db8192a8bececb80f3d789801ba3
pawlaczyk <pawlaczyk@g.pl>
2020-04-24 00:38:32 +0200

    dnspython

--- backend/DNS/a_record.py
+++ backend/DNS/a_record.py
@@ -1,6 +1,21 @@
+import dns.resolver
+
 class ARecord:
     """
+    pip install git+https://github.com/rthalley/dnspython
+    nazwa biblioteki i importu są inne -,- 
+    dlatego mogą być problemy z importmai xD
     https://www.tutorialspoint.com/python_network_programming/python_dns_look_up.htm
     """
     def __init__(self):
         pass
+
+
+if __name__ == "__main__":
+    result = dns.resolver.query('tutorialspoint.com', 'A')
+    for ipval in result:
+        print('IP', ipval.to_text())
+
+    result = dns.resolver.query('mail.google.com', 'CNAME')
+    for cnameval in result:
+        print (' cname target address:', cnameval.target)
