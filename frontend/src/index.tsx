504ebb0f54952c0b1c7ba4e96df90c9db177100a
Michal Pawlaczyk <michalm441@gmail.com>
2020-05-12 23:04:32 +0200

    Finished react configuration

---
+++ frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from 'views/Root';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
---
+++ frontend/src/theme/GlobalStyle.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700&display=swap&subset=latin-ext');
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  html {
+    font-size: 62.5%; 
+  }
+  
+  body {
+    margin: 0;
+    padding: 0;
+    font-size: 1.6rem;
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
