7920652113f37afdf4b725e453b331d52070ffb9
k-slonka <skarolina016@gmail.com>
2020-07-04 00:02:47 +0200

    |windows| computer name and system type

--- backend/local_installed/windows/hardware.py
+++ backend/local_installed/windows/hardware.py
@@ -3,28 +3,42 @@ import subprocess
 
 
 class Hardware:
-    def __init__(self):
-        pass
-    def get_bios(self)->str:
+    @staticmethod
+    def get_bios()->Dict:
         """
         Returns Bios version
         """
         value =  subprocess.getoutput("wmic bios get smbiosbiosversion")
-        print(value)
         response =  {
             "name" : value.split()[0],
             "version": value.split()[1]
         }
         return response
+
+    @staticmethod
+    def get_commputer_name()->Dict:
+        """
+        Returns name name_computer
+        """
+        name_computer =  subprocess.getoutput("wmic computersystem get name,systemtype")
+        response =  {
+            "name" : name_computer.split()[2],
+            "system_type": name_computer.split()[3]
+        }
+        return response
     
-    def get_another_command(self)->str:
+    @staticmethod
+    def get_another_command()->Dict:
         """
         Tutaj sobie implementujesz kolejne metody
         """
         pass
 
-    def to_json(self)->Dict:
+    @staticmethod
+    def to_json()->Dict:
         response = {}
-        response.update({"bios": self.get_bios()})
-        # response.update({"moja nazwa klucza": self.get_another_command()})
+        response.update({"bios": Hardware.get_bios()})
+        response.update({"computer_name": Hardware.get_commputer_name()})
+
+        # response.update({"moja nazwa klucza": Hardware.get_another_command()})
         return response  
