d74be659ff9708bd6fdae905f536bcdc855f17ac
k-slonka <skarolina016@gmail.com>
2020-10-20 21:19:36 +0200

    Report with data extracted from json.

--- backend/reports/host_info.py
+++ backend/reports/host_info.py
@@ -1,35 +1,36 @@
 from fpdf import FPDF, HTMLMixin
-
+import json
+import requests
 class PDF(FPDF, HTMLMixin):
     def header(self):
         self.set_font('Arial', 'B', 16)
 
-
-    def chapter(self):
+    def headerOnlyFirstSide(self):
         self.image('../../logo.png',90,15, 30)
-
         self.cell(0, 100, 'Host adress: 46.29.18.78', 0, 0, 'C')
         self.ln(60)
-        
+
+    def chapter(self):
+        response = requests.get("http://127.0.0.1:8000/search/censys/46.29.18.78")
+        searchDomainInfo = json.loads(response.text)
         epw = pdf.w - 2*pdf.l_margin
         col_width = epw/3
-        data = [['Protocols port',
-        'http: 80',
-        'imaps: 993',
-        'smtp: 465,25,587',
-        'pop3s: 995',
-        'pop3: 110',
-        'ftp: 21',
-        'imap: 143',
-        'https: 443',
-        'banner: 6666,873',
-        'Longitude: 21.0362']]
+        firstColumnTable = [['Protocols port',
+                'host: '+str(searchDomainInfo["protocols_port"]["http"][0]),
+                'imaps: '+str(searchDomainInfo["protocols_port"]["imaps"][0]),
+                'smtp: '+str(searchDomainInfo["protocols_port"]["smtp"][0])+", "+str(searchDomainInfo["protocols_port"]["smtp"][1])+", " +str(searchDomainInfo["protocols_port"]["smtp"][2]),
+                'pop3s: '+str(searchDomainInfo["protocols_port"]["pop3s"][0]),
+                'pop3: '+str(searchDomainInfo["protocols_port"]["pop3"][0]),
+                'ftp: '+str(searchDomainInfo["protocols_port"]["ftp"][0]),
+                'imap: '+str(searchDomainInfo["protocols_port"]["imap"][0]),
+                'https: '+str(searchDomainInfo["protocols_port"]["https"][0]),
+                'banner: '+str(searchDomainInfo["protocols_port"]["banner"][0])+', '+str(searchDomainInfo["protocols_port"]["banner"][1])+', '+str(searchDomainInfo["protocols_port"]["banner"][2]),
+                'Longitude: '+str(searchDomainInfo["longitude"])]]
         self.set_font('Times','',12.0) 
-        # self.set_font('Times','',10.0) 
         self.ln(0.5)
         th = pdf.font_size
 
-        for row in data:
+        for row in firstColumnTable:
             top = pdf.y
             for datum in row:
                 offset = pdf.x + col_width
@@ -37,19 +38,18 @@ class PDF(FPDF, HTMLMixin):
             self.ln(2*th)
 
 
-
-        data2 = [['Latitude: 52.2394',
-                'Timezone: Europe/Warsaw',
-                'Continent: Europe',
-                'Registered country: Poland,',
-                'Description: SPRINT-SDC',
-                'RIR: unknown',
-                'Routed prefix: 46.29.16.0/21']]
+        SecondColumnTable = [['Latitude:  '+str(searchDomainInfo["latitude"]),
+                'Timezone: '+str(searchDomainInfo["timezone"]),
+                'Continent: '+str(searchDomainInfo["continent"]),
+                'Registered country: '+str(searchDomainInfo["registered_country"]['PL']),
+                'Description: '+str(searchDomainInfo["description"]),
+                'RIR: '+str(searchDomainInfo["rir"]),
+                'Routed prefix: '+str(searchDomainInfo["routed_prefix"])]]
         self.set_font('Times','',12.0) 
         self.ln(0.5)
         th2 = pdf.font_size
        
-        for row in data2:
+        for row in SecondColumnTable:
             pdf.y = top
             for datum in row:
                 pdf.x = offset               
@@ -57,18 +57,16 @@ class PDF(FPDF, HTMLMixin):
             self.ln(2*th2)
 
 
-
-
-        data2 = [['Path: 7018,3257,197226',
-                'ASN: 197226',
-                'Name: SPRINT-SDC',
-                'DNS names: No data available',
-                'DNS errors: No data available',
-                'OS: No data available',
-                'Updated at: 2020-10-11T04:32:48+00:00']]
+        ThirdColumnTable = [['Path: '+str(searchDomainInfo["path"]),
+                'ASN: '+str(searchDomainInfo["asn"]),
+                'Name: '+str(searchDomainInfo["name"]),
+                'DNS names: '+str(searchDomainInfo["dns_names"]),
+                'DNS errors: '+str(searchDomainInfo["dns_erros"]),
+                'OS: '+str(searchDomainInfo["os"]),
+                'Updated at: '+str(searchDomainInfo["updated_at"])]]
         self.ln(0.5)
         self.set_font('Times','',12.0) 
-        for row in data2:
+        for row in ThirdColumnTable:
             pdf.y = top
             for datum in row:
                 pdf.x = offset+col_width               
@@ -77,26 +75,22 @@ class PDF(FPDF, HTMLMixin):
         self.ln(20)
 
 
-
-
-
-
-
         
-        data = [['Web page title: HostingHouse.pl',
-                'Web page body SHA256: b9b93998fa0c1b1f3c5da8469bb04254df54cd01060c8e035cfde18ecf1c1181',
-                'Status code: 200',
+        FirstColumnTable2 = [['Web page title: '+str(searchDomainInfo["https"]["webpage_title"]),
+                'Web page body SHA256: '+str(searchDomainInfo["https"]["webpage_body_sha256"]),
+                'Status code: '+str(searchDomainInfo["https"]["status_code"]),
                 'Metadata',
-                'Product: httpd',
-                'Version: No data available',
-                'Description: Apache httpd',
-                'Manufacturer: Apache']]
+                'Product: '+str(searchDomainInfo["https"]["get_metadata"]["product"]),
+                'Version: '+str(searchDomainInfo["path"]),
+                'Description: '+str(searchDomainInfo["path"]),
+                'Manufacturer: '+str(searchDomainInfo["path"])]]
+
         self.set_font('Times','',12.0) 
 
         self.ln(0.5)
         th = pdf.font_size
 
-        for row in data:
+        for row in FirstColumnTable2:
             top = pdf.y
             for datum in row:
                 offset = pdf.x + col_width
@@ -105,21 +99,21 @@ class PDF(FPDF, HTMLMixin):
 
 
 
-        data2 = [['RSA export: false',
-                'RSA length: No data available',
-                'RSA modulus: No data available',
-                'RSA exponent: No data available',
-                'DHE export: false',
+        SecondColumnTable2 = [['RSA export: '+str(searchDomainInfo["https"]["rsa_export"]),
+                'RSA length: '+str(searchDomainInfo["https"]["rsa_length"]),
+                'RSA modulus: '+str(searchDomainInfo["https"]["rsa_modulus"]),
+                'RSA exponent: '+str(searchDomainInfo["https"]["rsa_exponent"]),
+                'DHE export: '+str(searchDomainInfo["https"]["dhe_export"]),
                 'DH params',
-                'Prime length: No data available',
-                'Prime value: No data available',
-                'Generator length: No data available']]
+                'Prime length: '+str(searchDomainInfo["https"]["dh_params"]["prime_length"]),
+                'Prime value: '+str(searchDomainInfo["https"]["dh_params"]["prime_value"]),
+                'Generator length: '+str(searchDomainInfo["https"]["dh_params"]["generator_length"])]]
         self.set_font('Times','',12.0) 
 
         self.ln(0.5)
         th2 = pdf.font_size
        
-        for row in data2:
+        for row in SecondColumnTable2:
             pdf.y = top
             for datum in row:
                 pdf.x = offset               
@@ -127,18 +121,17 @@ class PDF(FPDF, HTMLMixin):
             self.ln(2*th2)
 
 
-
-
-        data2 = [['Generator value: No data available',
-                'DHE support: true',
-                'Heartbleed: true',
-                'Logjam attack: false',
-                'Freak attack: false',
-                'Poodle attack: No data available']]
+        ThirdColumnTable2 = [['Generator value: '+str(searchDomainInfo["https"]["dh_params"]["generator_value"]),
+                'DHE support: '+str(searchDomainInfo["https"]["dhe_support"]),
+                'Heartbleed: '+str(searchDomainInfo["https"]["heartbleed"]),
+                'Logjam attack: '+str(searchDomainInfo["https"]["logjam_attack"]),
+                'Freak attack: '+str(searchDomainInfo["https"]["freak_attack"]),
+                'Poodle attack: '+str(searchDomainInfo["https"]["poodle_attack"])
+                ]]
         self.set_font('Times','',12.0) 
         self.ln(0.5)
         th2 = pdf.font_size      
-        for row in data2:
+        for row in ThirdColumnTable2:
             pdf.y = top
             for datum in row:
                 pdf.x = offset+col_width               
@@ -147,83 +140,64 @@ class PDF(FPDF, HTMLMixin):
 
 
         self.ln(450)
-
-
-        # epw = pdf.w - 2*pdf.l_margin
     
 
-        data = [['Tbs noct fingerprint: 3e1a1a0f6c53f3e97a492d57084b5b9807059ee057ab1505876fd83fda3db838',
-                'Subject DN: C=US, O=Lets Encrypt, CN=Lets Encrypt Authority X3',
-                'Common name: Lets Encrypt Authority X3',
-                'Organization: Lets Encrypt',
-                'Organizational unit: No data available',
-                'Signature algorithm oid : 1.2.840.113549.1.1.11',
-                'Signature algorithm name: SHA256WithRSA',
-                'Redacted: false',
-                'Serial number: 13298795840390663119752826058995181320',
-                'alidation level: domain',
-                'Issuer DN: O=Digital Signature Trust Co., CN=DST Root CA X3',
-                'Fingerprint SHA1: e6a3b45b062d509b3382282d196efe97d5956ccb',
-                'Version: 3',
-                'Fingerprint SHA256: 25847d668eb4f04fdd40b12b6b0740c567da7d024308eb6c2c96fe41d9de218d',
-                'TBS fingerprint: 3e1a1a0f6c53f3e97a492d57084b5b9807059ee057ab1505876fd83fda3db838',
-                'Names: No data available',
-                'Validity start: 2016-03-17T16:40:46Z',
-                'Validity valid: No data available',
-                'Validity start: No data available',
-                'Fingerprint MD5: b15409274f54ad8f023d3b85a5ecec5d',
-                'Spki subject fingerprint: 78d2913356ad04f8f362019df6cb4f4f8b003be0d2aa0d1cb37d2fd326b09c9e',
-                'Subject key info fingerprint SHA256: 60b87575447dcba2a36b7d11ac09fb24a9db406fee12d2cc90180517616e8a18',
-                'Subject key info fingerprint algorithm name: RSA']]
+        Table3 = [['Tbs noct fingerprint: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["tbs_noct_fingerprint"]),
+                'Subject DN: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["subject_dn"]),
+                'Common name: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["common_name"][0]),
+                'Organization: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["organization"][0]),
+                'Organizational unit: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["organizational_unit"]),
+                'Signature algorithm oid: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["signature_algorithm_oid"]),
+                'Signature algorithm name: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["signature_algorithm_name"]),
+                'Redacted: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["redacted"]),
+                'Serial number: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["serial_number"]),
+                'alidation level: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["validation_level"]),
+                'Issuer DN: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["issuer_dn"]),
+                'Fingerprint SHA1: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["fingerprint_sha1"]),
+                'Version: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["version"]),
+                'Fingerprint SHA256: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["fingerprint_sha256"]),
+                'TBS fingerprint: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["tbs_fingerprint"]),
+                'Names '+str(searchDomainInfo["https"]["tls"]["chain"][0]["names"]),
+                'Validity start: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["validity_start"]),
+                'Validity valid: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["validity_valid"]),
+                'Validity start: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["validity_value"]),
+                'Fingerprint MD5: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["fingerprint_md5"]),
+                'Spki subject fingerprint: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["spki_subject_fingerprint"]),
+                'Subject key info fingerprint SHA256: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["subject_key_info_fingerprint_sha256"]),
+                'Subject key info fingerprint algorithm name: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["subject_key_info_key_algorithm_name"]),
+                'Subject key info fingerprint RSA public key length: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["subject_key_info_rsa_public_key_lenght"]),
+                'Subject key info fingerprint RSA public key modulus:  '+str(searchDomainInfo["https"]["tls"]["chain"][0]["subject_key_info_rsa_public_key_modulus"]),
+                'Subject key info fingerprint RSA public key exponent: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["subject_key_info_rsa_public_key_exponent"]),
+                'Signature self signed: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["signature_self_signed"]),
+                'Signature valid: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["signature_valid"]),
+                'Signature value: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["signature_value"]),
+                'Issuer organizational unit: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["issuer_organizational_unit"]),
+                'Issuer common name: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["issuer_common_name"][0]),
+                'ssuer organization: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["issuer_organization"][0]),
+                'Extensions',
+                'Authority key ID: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["extensions"]["authority_key_id"]),
+                'Certificate policies cps: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["extensions"]["certificate_policies_cps"]),
+                'Certificate policies ID: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["extensions"]["certificate_policies_id"]),
+                'Authority info access ocsp urls: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["extensions"]["authority_info_access_ocsp_urls"]),
+                'Authority info access issuer urls: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["extensions"]["authority_info_access_issuer_urls"]),
+                'Client auth: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["extensions"]["client_auth"]),
+                'Server auth: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["extensions"]["server_auth"]),
+                'DNS names: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["extensions"]["dns_names"]),
+                'IS CA: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["extensions"]["is_ca"]),
+                'CRL distribution points: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["extensions"]["crl_distribution_points"]),
+                'Key usage key encipherment: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["extensions"]["key_usage_key_encipherment"]),
+                'Key usage value: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["extensions"]["key_usage_value"]),
+                'Key usage is digital signature: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["extensions"]["key_usage_is_digital_signature"]),
+                'Subject key ID: '+str(searchDomainInfo["https"]["tls"]["chain"][0]["extensions"]["subject_key_id"])]]
         self.set_font('Times','',12.0) 
         self.ln(0.5)
         th = pdf.font_size
 
-        for row in data:
+        for row in Table3:
             top = pdf.y
             for datum in row:
                 self.multi_cell(epw, 1.6*th, str(datum),0,0,'L')
-
-
-
-        data2 = [['Subject key info fingerprint RSA public key length: 2048',
-                'Subject key info fingerprint RSA public key modulus: nNMM8FrlLke3cl03g7NoYzDq1zUmGSXhvb418XCSL7e4S0EFq6meNQhY7LEqxGiHC6PjdeTm86dicbp5gWAf15Gan/PQeGdxyGkOlZHP/uaZ6WA8SMx+yk13EiSdRxta67nsHjcAHJyse6cF6s5K671B5TaYucv9bTyWaN8jKkKQDIZ0Z8h/pZq4UmEUEz9l6YKHy9v6Dlb2honzhT+Xhq+w3Brvaw2VFn3EK6BlspkENnWAa6xK8xuQSXgvopZPKiAlKQTGdMDQMc2PMTiVFrqoM7hD8bEfwzB/onkxEz0tNvjj/PIzark5McWvxI0NHWQWM6r6hCm21AvA2H3Dkw==',
-                'Subject key info fingerprint RSA public key exponent: 65537',
-                'Signature self signed: false',
-                'Signature valid: true',
-                'Signature value: 3TPXEfNjWDjdGBX7CVW+dla5cEilaUcne8IkCJLxWh9KEik3JHRRHGJouM2VcGfl96S8TihRzZvoroed6ti6WqEBmtzw3Wodatg+VyOeph4EYpr/1wXKtx8/wApIvJSwtmVi4MFU5aMqrSDE6ea73Mj2tcMyo5jMd6jmeWUHK8so/joWUoHOUgwuX4Po1QYz+3dszkDqMp4fklxBwXRsW10KXzPMTZ+sOPAveyxindmjkW8lGy+QsRlGPfZ+G6Z6h7mjem0Y+iWlkYcV4PIWL1iwBi8saCbGS5jN2p8M+X+Q7UNKEkROb3N6KOqkqm57TH2H3eDJAkSnh6/DNFu0Qg==',
-                'Issuer organizational unit: No data available',
-                'Issuer common name: DST Root CA X3',
-                'ssuer organization: Digital Signature Trust Co.',
-                'Extensions',
-                'Authority key ID: c4a7b1a47b2c71fadbe14b9075ffc41560858910',
-                'Certificate policies cps: http://cps.root-x1.letsencrypt.org',
-                'Certificate policies ID: 2.23.140.1.2.1,1.3.6.1.4.1.44947.1.1.1',
-                'Authority info access ocsp urls: http://isrg.trustid.ocsp.identrust.com',
-                'Authority info access issuer urls: http://apps.identrust.com/roots/dstrootcax3.p7c',
-                'Client auth: No data available',
-                'Server auth: No data available',
-                'DNS names: No data available',
-                'IS CA: true',
-                'CRL distribution points: http://crl.identrust.com/DSTROOTCAX3CRL.crl',
-                'Key usage key encipherment: No data available',
-                'Key usage value: 97',
-                'Key usage is digital signature:',
-                'Subject key ID: a84a6a63047dddbae6d139b7a64565eff3a8eca1']]
-        self.set_font('Times','',12.0) 
-        self.ln(0.5)
-        th2 = pdf.font_size
-        for row in data2:
-            top = pdf.y
-            for datum in row:
-                self.multi_cell(epw, 1.6*th, str(datum),0,0,'L')
-            self.ln(2*th2)
-
-
-
-
-
-    
+                
     def footer(self):
         self.set_y(-15)
         self.set_font('Arial', 'I', 8)
@@ -233,10 +207,10 @@ class PDF(FPDF, HTMLMixin):
         
 
 
-
 pdf = PDF()
 pdf.alias_nb_pages()
 pdf.add_page()
+pdf.headerOnlyFirstSide()
 pdf.chapter()
 pdf.set_font('Times', '', 12)
 pdf.output('report_host_info.pdf', 'F')
