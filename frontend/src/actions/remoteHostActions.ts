a22cc26d389e4842afd0a30fd99af4c73c62595b
Michal Pawlaczyk <michalm441@gmail.com>
2020-11-20 12:58:26 +0100

    [SAR-162] Added report button

--- frontend/src/actions/remoteHostActions.ts
+++ frontend/src/actions/remoteHostActions.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { serverRoutes } from 'routes';
 
 export const actions: Record<string, string> = {
   FETCH_DATA_REQUEST: 'FETCH_HOST_INFO_REQUEST',
@@ -12,16 +13,18 @@ export const fetchData = (searchHost: string, tabIndex: number) => (
   dispatch({
     type: actions.FETCH_DATA_REQUEST,
     payload: {
+      searchedHost: searchHost,
       tabIndex,
     },
   });
 
   return axios
-    .get(`http://localhost:8000/search/censys/${searchHost}`)
+    .get(`${serverRoutes.remoteHostData}${searchHost}`)
     .then(({ data }) => {
       dispatch({
         type: actions.FETCH_DATA_SUCCESS,
         payload: {
+          searchedHost: searchHost,
           tabIndex,
           data,
         },
@@ -31,9 +34,15 @@ export const fetchData = (searchHost: string, tabIndex: number) => (
       dispatch({
         type: actions.FETCH_DATA_FAILURE,
         payload: {
+          searchedHost: searchHost,
           tabIndex,
           error: err.message,
         },
       });
     });
+};
+
+export const fetchReport = (reportForIp: string) => {
+  const URL = `${serverRoutes.remoteHostReport}${reportForIp}`;
+  window.open(URL);
 };
