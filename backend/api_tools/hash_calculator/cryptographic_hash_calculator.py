8f5a6b43e56ed24159bf18ab440e41715de06e1b
pawlaczyk <pawlaczyk@g.pl>
2020-12-12 20:12:20 +0100

    [hash_calculator] cryptographic shash calcualtor end

--- backend/api_tools/hash_calculator/cryptographic_hash_calculator.py
+++ backend/api_tools/hash_calculator/cryptographic_hash_calculator.py
@@ -20,11 +20,11 @@ rodzaje BLAKE2
     BLAKE2s
     BLAKE2b
 """
+from typing import Dict
 import base64
 from Crypto.Hash import SHA256, SHA224, SHA3_224, SHA3_256, SHA3_384,  SHA384, SHA512, BLAKE2s
-
-
 import pprint
+
 
 class CryptographicHashCalcualator:
     """
@@ -39,16 +39,11 @@ class CryptographicHashCalcualator:
     def byte_string(self):
         return str.encode(self.value, "utf-8")
 
-    @property
-    def bytearray_value(self):
-        return bytearray(self.value)
-
     def get_sha224(self):
         hash_obj = SHA224.new(data=self.value)
-        print("Biciki: ",hash_obj.digest() )
         return {
-            "digest": base64.b64encode(hash_obj.digest()),
-            "hexdigets": str(hash_obj.hexdigest())
+            "digest": hash_obj.digest(),
+            "hexdigets": hash_obj.hexdigest()
         }
 
     def get_sha256(self):
@@ -147,22 +142,176 @@ class CryptographicHashCalcualator:
         mac.update(self.value)
         return mac.digest(), mac.hexdigest()
 
-    def get_data(self):
+
+class CryptographicHashValues: #TODO: refaktor
+    """Wrapper na dane z CryptographicHashCalcualator ponieważ wystepują problemy z kodowaniem.
+    Pomocnicza klasa zamiast serializera dla widoku"""
+    def __init__(self, value:str):
+        self.value = value.encode("utf-8")
+        self.crypto_hash_calc = CryptographicHashCalcualator(value)
+
+    def __convert_digest(self, digest):
+        return base64.b64encode(digest)
+
+    # sha224
+    @property
+    def sha224_digest(self):
+        digest = self.crypto_hash_calc.get_sha224()["digest"]
+        return self.__convert_digest(digest)
+
+    @property
+    def sha224_hexdigets(self):
+        return self.crypto_hash_calc.get_sha224()["hexdigets"]
+
+    # sha256
+    @property
+    def sha256_digest(self):
+        digest = self.crypto_hash_calc.get_sha256()["digest"]
+        return self.__convert_digest(digest)
+
+    @property
+    def sha256_hexdigets(self):
+        return self.crypto_hash_calc.get_sha256()["hexdigets"]
+
+    #sha3_224
+    @property
+    def sha3_224_digest(self):
+        digest = self.crypto_hash_calc.get_sha3_224()["digest"]
+        return self.__convert_digest(digest)
+
+    @property
+    def sha3_224_hexdigets(self):
+        return self.crypto_hash_calc.get_sha3_224()["hexdigets"]
+
+    #sha3_256
+    @property
+    def sha3_256_digest(self):
+        digest = self.crypto_hash_calc.get_sha3_256()["digest"]
+        return self.__convert_digest(digest)
+
+    @property
+    def sha3_256_hexdigets(self):
+        return self.crypto_hash_calc.get_sha3_256()["hexdigets"]
+
+    #sha3_384
+    @property
+    def sha3_384_digest(self):
+        digest = self.crypto_hash_calc.get_sha3_384()["digest"]
+        return self.__convert_digest(digest)
+
+    @property
+    def sha3_384_hexdigets(self):
+        return self.crypto_hash_calc.get_sha3_384()["hexdigets"]
+
+    #sha384
+    @property
+    def sha384_digest(self):
+        digest = self.crypto_hash_calc.get_sha384()["digest"]
+        return self.__convert_digest(digest)
+
+    @property
+    def sha384_hexdigets(self):
+        return self.crypto_hash_calc.get_sha384()["hexdigets"]
+
+    #sha512
+    @property
+    def sha512_digest(self):
+        digest = self.crypto_hash_calc.get_sha512()["digest"]
+        return self.__convert_digest(digest)
+
+    @property
+    def sha512_hexdigets(self):
+        return self.crypto_hash_calc.get_sha512()["hexdigets"]
+
+    @property
+    def sha512_warning(self):
+        return self.crypto_hash_calc.get_sha512()["warning"]
+
+    # sha512_truncate_224
+    @property
+    def sha512_truncate_224_digest(self):
+        digest = self.crypto_hash_calc.get_sha512_truncate_224()["digest"]
+        return self.__convert_digest(digest)
+
+    @property
+    def sha512_truncate_224_hexdigets(self):
+        return self.crypto_hash_calc.get_sha512_truncate_224()["hexdigets"]
+
+    @property
+    def sha512_truncate_224_warning(self):
+        return self.crypto_hash_calc.get_sha512_truncate_224()["warning"]
+
+    # sha512_truncate_256
+    @property
+    def sha512_truncate_256_digest(self):
+        digest = self.crypto_hash_calc.get_sha512_truncate_256()["digest"]
+        return self.__convert_digest(digest)
+
+    @property
+    def sha512_truncate_256_hexdigets(self):
+        return self.crypto_hash_calc.get_sha512_truncate_256()["hexdigets"]
+
+    @property
+    def sha512_truncate_256_warning(self):
+        return self.crypto_hash_calc.get_sha512_truncate_256()["warning"]
+
+    # blake2s_256_bits
+    @property
+    def blake2s_256_bits_digest(self):
+        digest = self.crypto_hash_calc.get_blake2s_256_bits()["digest"]
+        return self.__convert_digest(digest)
+
+    @property
+    def blake2s_256_bits_hexdigets(self):
+        return self.crypto_hash_calc.get_blake2s_256_bits()["hexdigets"]
+
+    def get_data(self)->Dict:
+        """Zwraca jsona który może być zwrócocny bezpoeśrednio przez widok Django."""
         return {
-            "sha224": self.get_sha224(),
-            # "sha256": self.get_sha256(),
-            # "sha3_224": self.get_sha3_224(),
-            # "sha3_256": self.get_sha3_256(),
-            # "sha3_384": self.get_sha3_384(),
-            # "sha384": self.get_sha384(),
-            # "sha512": self.get_sha512(),
-            # "sha512_truncate_224": self.get_sha512_truncate_224(),
-            # "sha512_truncate_256": self.get_sha512_truncate_256(),
-            # "blake2s_256_bits": self.get_blake2s_256_bits(),
+            # sha224
+            "sha224_digest": self.sha224_digest,
+            "sha224_hexdigets": str(self.sha224_hexdigets),
 
+            # sha256
+            "sha256_digest": self.sha256_digest,
+            "sha256_hexdigets": self.sha256_hexdigets,
 
-            # "get_blake2s_as_mac": self.get_blake2s_as_mac() #TODO
+            # sha3_224
+            "sha3_224_digest": self.sha3_224_digest,
+            "sha3_224_hexdigets": self.sha3_224_hexdigets,
+
+            # sha3_256
+            "sha3_256_digest": self.sha3_256_digest,
+            "sha3_256_hexdigets": self.sha3_256_hexdigets,
+
+            # sha3_384
+            "sha3_384_digest": self.sha3_384_digest,
+            "sha3_384_hexdigets": self.sha3_384_hexdigets,
+
+            # sha384
+            "sha384_digest": self.sha384_digest,
+            "sha384_hexdigets": self.sha384_hexdigets,
+
+            # sha512
+            "sha512_digest": self.sha512_digest,
+            "sha512_hexdigets": self.sha512_hexdigets,
+            "sha512_warning": self.sha512_warning,
+
+            # sha512_truncate_224
+            "sha512_truncate_224_digest": self.sha512_truncate_224_digest,
+            "sha512_truncate_224_hexdigets": self.sha512_truncate_224_hexdigets,
+            "sha512_truncate_224_warning": self.sha512_truncate_224_warning,
+
+            # sha512_truncate_256
+            "sha512_truncate_256_digest": self.sha512_truncate_256_digest,
+            "sha512_truncate_256_hexdigets": self.sha512_truncate_256_hexdigets,
+            "sha512_truncate_256_warning": self.sha512_truncate_256_warning,
+
+            # blake2s_256_bits
+            "blake2s_256_bits_digest": self.blake2s_256_bits_digest,
+            "blake2s_256_bits_hexdigets": self.blake2s_256_bits_hexdigets
         }
+
 
 
 if __name__ == "__main__":
