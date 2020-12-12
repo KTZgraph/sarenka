126fb6ca3620d582cf3cb859e82430dffe0b04fb
pawlaczyk <pawlaczyk@g.pl>
2020-12-12 21:12:04 +0100

    [SAR-174] shanon entropy

--- backend/api_tools/urls.py
+++ backend/api_tools/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HashCalcualtorView
+from .views import HashCalcualtorView, EntropyCalculatorView
 
 urlpatterns = [
-    path('hash_calculator/<str:value>', HashCalcualtorView.as_view(), name="hash_calculator"),
+    path('hash/<str:value>', HashCalcualtorView.as_view(), name="hash_calculator"),
+    path('entropy/<str:value_sequence>', EntropyCalculatorView.as_view(), name="entropy_calculator"),
 ]
