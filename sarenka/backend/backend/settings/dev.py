c2ff8eb958d65cffbf76d6e5177c96a3419143dc
pawlaczyk <pawlaczyk@g.pl>
2020-12-27 22:36:15 +0100

    create logs dir and files

--- sarenka/backend/backend/settings/dev.py
+++ sarenka/backend/backend/settings/dev.py
@@ -1,4 +1,25 @@
+from pathlib import Path
+
 from backend.settings.base import *
+
+# class LogFileError(Exception):
+
+# gdy plik logów nie istnieje
+if not Path("logs").is_dir():
+    dirname= Path('logs')
+    dirname.mkdir()
+
+
+if not Path('info_dev1.log').is_file():
+    filename = Path('logs/info_dev1.log')
+    filename.touch(exist_ok=True)  # will create file, if it exists will do nothing
+
+if not Path('debug_dev1.log').is_file():
+    filename = Path('logs/debug_dev1.log')
+    filename.touch(exist_ok=True)  # will create file, if it exists will do nothing
+
+
+
 
 LOGGING = {
     "version": 1,
@@ -12,13 +33,13 @@ LOGGING = {
         "file": {
             "level": "INFO",
             "class": "logging.FileHandler",
-            "filename": "./logs/info_dev1.log",
+            "filename": "./logs/info_dev.log",
             "formatter": "simple_formatter",
         },
         "file2": {
             "level": "DEBUG",
             "class": "logging.FileHandler",
-            "filename": "./logs/debug_dev1.log",
+            "filename": "./logs/debug_dev.log",
             "formatter": "simple_formatter2",
         }
     },
