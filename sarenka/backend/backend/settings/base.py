863e7cbc3d7522507e9a6586fb7230b883e57e1e
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2021-01-11 23:48:32 +0100

    CORS fix

--- sarenka/backend/backend/settings/base.py
+++ sarenka/backend/backend/settings/base.py
@@ -53,10 +53,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ORIGIN_WHITELIST = [
-#     "http://localhost:3000",
-#     "http://127.0.0.1:3000"
-# ]
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
+]
 
 CORS_ALLOW_METHODS = [
     'GET',
