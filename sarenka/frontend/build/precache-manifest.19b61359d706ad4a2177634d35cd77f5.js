d18fd39115d63d3c276850fadad355498c6a8bbc
Michal Pawlaczyk <michalm441@gmail.com>
2021-01-15 13:37:49 +0100

    Updated build folder

---
+++ sarenka/frontend/build/precache-manifest.19b61359d706ad4a2177634d35cd77f5.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9793f6ae59bfa7b0e482f7a897305837",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcbb2089f4d9f4b9d053",
+    "url": "/static/js/10.77db1d1b.chunk.js"
+  },
+  {
+    "revision": "e888d08d9cc54f07c745",
+    "url": "/static/js/11.7ca7da62.chunk.js"
+  },
+  {
+    "revision": "92a8128763cc2b31c786",
+    "url": "/static/js/12.0efe2bab.chunk.js"
+  },
+  {
+    "revision": "ef802884666b66d9cd24",
+    "url": "/static/js/2.e73d4414.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e73d4414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94226f32555b2760552f",
+    "url": "/static/js/3.a3475371.chunk.js"
+  },
+  {
+    "revision": "1a0c0472ee46f86ca37d",
+    "url": "/static/js/4.070380d1.chunk.js"
+  },
+  {
+    "revision": "4ef796c54aaf1ed7990d",
+    "url": "/static/js/5.e8a9c086.chunk.js"
+  },
+  {
+    "revision": "1662765d70555832036a",
+    "url": "/static/js/6.708c842f.chunk.js"
+  },
+  {
+    "revision": "9788213286360f1d1ea9",
+    "url": "/static/js/7.b4bfa672.chunk.js"
+  },
+  {
+    "revision": "f3c83f0340e27793a963",
+    "url": "/static/js/8.e215140e.chunk.js"
+  },
+  {
+    "revision": "0d1f83b4b2dfc1b0a34e",
+    "url": "/static/js/9.368e5b12.chunk.js"
+  },
+  {
+    "revision": "3f58c5a4b000d847fcf2",
+    "url": "/static/js/main.e1faabe2.chunk.js"
+  },
+  {
+    "revision": "bc58c02494a423033444",
+    "url": "/static/js/runtime-main.4e60b914.js"
+  },
+  {
+    "revision": "ad645f95c60a82c7d9ebeae3b5f3ddcf",
+    "url": "/static/media/arrow-right.ad645f95.svg"
+  },
+  {
+    "revision": "dcb77b0249ef3a37132cdb5de06bed6a",
+    "url": "/static/media/docsIcon.dcb77b02.svg"
+  },
+  {
+    "revision": "1143909ee3912944e757d3c65ff34d3e",
+    "url": "/static/media/docsIconActive.1143909e.svg"
+  },
+  {
+    "revision": "054838d3e08404b1da1365c98903362e",
+    "url": "/static/media/exploitsIcon.054838d3.svg"
+  },
+  {
+    "revision": "e3dcda322d7eca2d2ee9c8bd9fc93095",
+    "url": "/static/media/exploitsIconActive.e3dcda32.svg"
+  },
+  {
+    "revision": "b87eaf474c61dd786bb909af246bb8af",
+    "url": "/static/media/externalLinkIcon.b87eaf47.svg"
+  },
+  {
+    "revision": "151ff5695a9fa35ac1465ca4751d1d3e",
+    "url": "/static/media/hardwareIcon.151ff569.svg"
+  },
+  {
+    "revision": "31d7f9619f7f0df9b3799a1d5ff9c6ca",
+    "url": "/static/media/hardwareIconActive.31d7f961.svg"
+  },
+  {
+    "revision": "a395ec1937fe9ba3ab4f16a7959defe1",
+    "url": "/static/media/logo.a395ec19.svg"
+  },
+  {
+    "revision": "f3fcbe1550807fa12cd008494bbba2d1",
+    "url": "/static/media/magnifierIcon.f3fcbe15.svg"
+  },
+  {
+    "revision": "3d6aa3627fdf80eeeec410f1049aa3c2",
+    "url": "/static/media/magnifierIconActive.3d6aa362.svg"
+  },
+  {
+    "revision": "2c70ca155bb8b2f1922c48f322d56436",
+    "url": "/static/media/magnifierIconInput.2c70ca15.svg"
+  },
+  {
+    "revision": "6d5bb378f2dec07c8266be7052fc938e",
+    "url": "/static/media/regeditIcon.6d5bb378.svg"
+  },
+  {
+    "revision": "77ba7706a201a9589eb23b7105d31ff8",
+    "url": "/static/media/regeditIconActive.77ba7706.svg"
+  },
+  {
+    "revision": "246d0bad8a3d5225a70678ce0e91abbf",
+    "url": "/static/media/removeIcon.246d0bad.svg"
+  },
+  {
+    "revision": "063560a42a184aea80e5251d4de40754",
+    "url": "/static/media/resultbackground.063560a4.png"
+  },
+  {
+    "revision": "377ad9b33a0e18320e1e2a748bf9ad5b",
+    "url": "/static/media/searchBackground.377ad9b3.png"
+  },
+  {
+    "revision": "2800431f15e1eedf0f116b3661a463ac",
+    "url": "/static/media/settingsBackground.2800431f.png"
+  },
+  {
+    "revision": "93cf2ed2bc86ab14e04e01896b386ab5",
+    "url": "/static/media/settingsIcon.93cf2ed2.svg"
+  },
+  {
+    "revision": "cc0c85238b99021923e601e5b0f632ff",
+    "url": "/static/media/settingsIconActive.cc0c8523.svg"
+  },
+  {
+    "revision": "7b44993b8162375c27660d7029c9f928",
+    "url": "/static/media/warningIcon.7b44993b.svg"
+  }
+]);
