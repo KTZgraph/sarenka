210286d6898b90876c8a03fcba0e201b65e0c4cb
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2021-04-28 00:39:27 +0200

    menu favicon, first reds

--- src/frontend/src/components/Menu.vue
+++ src/frontend/src/components/Menu.vue
@@ -2,43 +2,52 @@
   <nav class="menu-main active">
     <ul>
       <li>
-        <router-link to="/sources" active-classes="active" class="nav-link">Sources</router-link>
+        <router-link to="/home" active-classes="active" class="nav-link">Home</router-link>
+        <div class="menu-ico"><i class="fas fa-heart"></i></div>
       </li>
-
+      <li>
+        <router-link to="/sources" active-classes="active" class="nav-link">Sources</router-link>
+        <div class="menu-ico"><i class="fas fa-table"></i></div>
+      </li>
       <li>
         <router-link to="/products" active-classes="active" class="nav-link">Products</router-link>
+        <div class="menu-ico"><i class="fas fa-laptop-code"></i></div>
       </li>
-
       <li>
         <router-link to="/search" active-classes="active" class="nav-link">Search</router-link>
+        <div class="menu-ico"><i class="fas fa-search"></i></div>
       </li>
-
+      <li>
+        <router-link to="/people" active-classes="active" class="nav-link">Search</router-link>
+        <div class="menu-ico"><i class="fas fa-user"></i></div>
+      </li>
       <li>
         <router-link to="/technologies" active-classes="active" class="nav-link">Technologies</router-link>
+        <div class="menu-ico"><i class="fas fa-digital-tachograph"></i></div>
       </li>
-
       <li>
         <router-link to="/tools" active-classes="active" class="nav-link">Tools</router-link>
+        <div class="menu-ico"><i class="fas fa-tools"></i></div>
       </li>
-
       <li>
         <router-link to="/vendors" active-classes="active" class="nav-link">Vendors</router-link>
+        <div class="menu-ico"><i class="fas fa-building"></i></div>
       </li>
-
       <li>
         <router-link to="/cves" active-classes="active" class="nav-link">CVEs</router-link>
+        <div class="menu-ico"><i class="fas fa-fire-alt"></i></div>
       </li>
-
       <li>
         <router-link to="/cwes" active-classes="active" class="nav-link">CWEs</router-link>
+        <div class="menu-ico"><i class="fas fa-fire-alt"></i></div>
       </li>
-
       <li>
-        <router-link to="/cwes-top-25" active-classes="active" class="nav-link">CWEs Top 25</router-link>
+        <router-link to="/cwes-top-25" active-classes="active" class="nav-link">Top CWEs</router-link>
+        <div class="menu-ico"><i class="fas fa-fire-alt"></i></div>
       </li>
-
       <li>
         <router-link to="/cheat-sheets" active-classes="active" class="nav-link">Cheat Sheet</router-link>
+        <div class="menu-ico"><i class="far fa-file-alt"></i></div>
       </li>
     </ul>
     <button class="menu-main active"><i class="fas fa-arrow-left menu-main"></i></button>
