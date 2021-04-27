4b64221aa3318abecdc47d3cb230027fe3ad7adc
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2021-04-27 22:08:25 +0200

    header proportion

--- src/frontend/src/components/Header.vue
+++ src/frontend/src/components/Header.vue
@@ -1,21 +1,27 @@
 <template>
-    <header class="header-main">
-      <router-link to="/documentation" class="header-url"><div><img src="../assets/logo_crop.png" alt="sarenka logo"></div></router-link>
-      <router-link to="/settings" class="header-url"><i class="fas fa-cog"></i><strong>Settings</strong></router-link>
-    </header>
+  <header class="header-main">
+    <router-link to="/documentation" class="header-url">
+      <div class="header-logo"><img src="../assets/logo_crop_name.png" alt="sarenka logo"></div>
+    </router-link>
+    <router-link to="/search" class="header-url">
+      <div class="header-search"><input type="text" placeholder="search"></div>
+    </router-link>
+    <router-link to="/settings" class="header-url"><div class="header-settings"><i class="fas fa-cog"></i>Settings</div></router-link>
+  </header>
 </template>
 
 <script>
 import {useRouter} from 'vue-router';
-import { useStore } from 'vuex';
-export default {
-    name: "Header",
-    
-    setup(){
-      const router = useRouter();
-      const store = useStore();
+import {useStore} from 'vuex';
 
-    
-    }
+export default {
+  name: "Header",
+
+  setup() {
+    const router = useRouter();
+    const store = useStore();
+
+
+  }
 }
 </script>
