6de024344e0ce45ce5fcbf297aa83f79a308db50
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2021-12-18 20:54:37 +0100

    cwe top 25 front data

--- src/frontend/pages/vulns/cwe/index.js
+++ src/frontend/pages/vulns/cwe/index.js
@@ -1,8 +1,38 @@
-const CWE = () => {
+import Link from 'next/link';
+import Head from "next/head";
+
+export const getStaticProps = async () => {
+    const rest = await fetch('http://127.0.0.1:8000/api/vulns/cwe-top/');
+    const data = await rest.json();
+
+    return {
+        props: {cweTopList: data} 
+    }
+}
+
+
+const CWE = ({cweTopList}) => {
     return (
+        <>
+        <Head>
+            {/* komponent na metadane np title */}
+            <title>Sarenka | CWE TOP 25 </title>
+            <meta name='keywords' content='ninjas'/>
+        </Head>
+        
         <div>
-            <h1>TOP 25 CVE</h1>
+            <h1>TOP 25 CWE</h1>
+            <section>
+            {cweTopList.map(cwe => (
+              <Link href={'/vulns/cwe/' + cwe.id} key={cwe.id}>
+                  <a>
+                      <h3>{cwe.name}</h3>
+                  </a>
+              </Link>
+            ))}
+            </section>
         </div>
+        </>
     );
 }
  
