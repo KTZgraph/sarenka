600565cf251a9965bb67fa90a73f9c27b017b8b0
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2021-12-30 21:07:21 +0100

    download and unzip feed files

--- src/backend/script_copy/cve_list_load.py
+++ src/backend/script_copy/cve_list_load.py
@@ -7,8 +7,12 @@ załadowanie danych:
 python manage.py runscript cve_list_load # BEZ ROZSZRZERZENIA PY
 """
 from bs4 import BeautifulSoup
-import requests
+from requests import get
+from pathlib import Path
+from zipfile import ZipFile
+from os import remove
 
+DIR_PATH = Path(__file__).parent.absolute()
 MAIN_URL = "https://nvd.nist.gov" # bez / na końcu
 
 feeder_page = "https://nvd.nist.gov/vuln/data-feeds#JSON_FEED"
@@ -16,7 +20,7 @@ feeder_page = "https://nvd.nist.gov/vuln/data-feeds#JSON_FEED"
 
 def scrap()->list:
     files_urls = []
-    source  = requests.get(feeder_page).text
+    source  = get(feeder_page).text
     soup = BeautifulSoup(source, 'lxml')
 
     # <div id="divJSONFeeds" class="row">
@@ -32,8 +36,65 @@ def scrap()->list:
     return files_urls
 
 
-def download(files_urls:list):
-    pass
+def download(files_urls:list)->list:
+    zip_filenames = []
+    
+    for file_url in files_urls:
+        zip_filename = file_url.split('/')[-1]
+        data  = get(file_url)
+        with open(DIR_PATH.joinpath(zip_filename), 'wb') as f:
+            f.write(data.content)
+            zip_filenames.append(zip_filename)
+    
+    return zip_filenames
+
+def unzip(zip_filenames:list)->list:
+    json_filenames = []
+
+    for zip_filename in zip_filenames:
+        zip_filename = DIR_PATH.joinpath(zip_filename)
+
+        with ZipFile(zip_filename, 'r') as zip_ref:
+            extracted_file_name = zip_ref.namelist()
+            zip_ref.extractall(DIR_PATH)
+
+        remove(zip_filename)
+        json_filenames.append(extracted_file_name[0]) #tylko jeden plik w zipie
+
+    return json_filenames
+
+def run()->list:
+    # files_urls = scrap()
+    # zip_filenames = download(files_urls)
+    # json_filenames = unzip(zip_filenames)
+    json_filenames = [
+    'nvdcve-1.1-modified.json', 
+    'nvdcve-1.1-recent.json', 
+    'nvdcve-1.1-2021.json', 
+    'nvdcve-1.1-2020.json', 
+    'nvdcve-1.1-2019.json', 
+    'nvdcve-1.1-2018.json', 
+    'nvdcve-1.1-2017.json', 
+    'nvdcve-1.1-2016.json', 
+    'nvdcve-1.1-2015.json', 
+    'nvdcve-1.1-2014.json', 
+    'nvdcve-1.1-2013.json', 
+    'nvdcve-1.1-2012.json', 
+    'nvdcve-1.1-2011.json', 
+    'nvdcve-1.1-2010.json', 
+    'nvdcve-1.1-2009.json', 
+    'nvdcve-1.1-2008.json', 
+    'nvdcve-1.1-2007.json', 
+    'nvdcve-1.1-2006.json', 
+    'nvdcve-1.1-2005.json', 
+    'nvdcve-1.1-2004.json', 
+    'nvdcve-1.1-2003.json', 
+    'nvdcve-1.1-2002.json'
+    ]
+
+    json_filenames.sort() #modified and recent on the end
+    for f in json_filenames:
+        print(f)
 
 if __name__ =="__main__":
-    scrap()
+    run()
--- src/backend/script_copy/cwe_list_load.py
+++ src/backend/script_copy/cwe_list_load.py
@@ -17,7 +17,7 @@ from apps.vulnerabilities.models import CWE
 
 DIR_PATH = Path(__file__).parent.absolute()
 
-def download():
+def download()->str:
     file_url = 'https://cwe.mitre.org/data/xml/cwec_latest.xml.zip'
     zip_filename = file_url.split('/')[-1]
     data  = get(file_url)
@@ -25,7 +25,7 @@ def download():
         f.write(data.content)
     return zip_filename
 
-def unzip(zip_filename:str):
+def unzip(zip_filename:str)->str:
     zip_filename = DIR_PATH.joinpath(zip_filename)
     with ZipFile(zip_filename, 'r') as zip_ref:
         extracted_file_name = zip_ref.namelist()
@@ -34,7 +34,7 @@ def unzip(zip_filename:str):
     remove(zip_filename)
     return extracted_file_name[0] # tylko jeden plik w zipie
 
-def parse(extracted_file_name:str):
+def parse(extracted_file_name:str)->list:
     cwe_list = []
     extracted_file_name = str(DIR_PATH.joinpath(extracted_file_name))
 
@@ -68,7 +68,7 @@ def parse(extracted_file_name:str):
     remove(extracted_file_name)
     return cwe_list
 
-def run():
+def run()->None:
     zip_filename = download()
     extracted_file_name = unzip(zip_filename)
     cwe_list = parse(extracted_file_name)
