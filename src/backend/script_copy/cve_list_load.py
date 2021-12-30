a4f317ada682b9897dc0cd6c8f828bc4eacb907b
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2021-12-30 20:38:36 +0100

    cve json files urls

--- src/backend/script_copy/cve_list_load.py
+++ src/backend/script_copy/cve_list_load.py
@@ -6,11 +6,34 @@ stworzyć folder srcripts a w nim scripts/__init__.py pusty
 załadowanie danych:
 python manage.py runscript cve_list_load # BEZ ROZSZRZERZENIA PY
 """
+from bs4 import BeautifulSoup
+import requests
 
-MAIN_URL = "https://nvd.nist.gov/vuln/data-feeds#JSON_FEED"
+MAIN_URL = "https://nvd.nist.gov" # bez / na końcu
 
-file_url = "https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-modified.json.zip"
+feeder_page = "https://nvd.nist.gov/vuln/data-feeds#JSON_FEED"
 
 
-def download():
+def scrap()->list:
+    files_urls = []
+    source  = requests.get(feeder_page).text
+    soup = BeautifulSoup(source, 'lxml')
+
+    # <div id="divJSONFeeds" class="row">
+    soup = soup.find("div", {"id": "divJSONFeeds"})
+    # <table class="xml-feed-table table table-striped table-condensed">
+    soup = soup.find("table") #tylko jedna tabela w sordku diva 
+
+    for a in soup.find_all("a", href=True):
+        href = a["href"]
+        if href.split('.')[-1] == "zip":
+            files_urls.append(MAIN_URL + href)
+    
+    return files_urls
+
+
+def download(files_urls:list):
     pass
+
+if __name__ =="__main__":
+    scrap()
