4538bad11af20a446dbf3478b237b3d08c4466c8
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-08 10:33:23 +0100

    tła + style dla body

--- src/frontend/pages/index.js
+++ src/frontend/pages/index.js
@@ -2,10 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+function HomePage() {
   return (
-    <>
-    
-    </>
+    <div className="container">
+      <h1>Container</h1>
+
+    </div>
   )
 }
+
+
+export default HomePage;
