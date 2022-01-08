0f044e3bf84b09095ebd2ead29f58288606e904b
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-08 14:02:21 +0100

    dodanie ogólnego title i meta

--- src/frontend/pages/_app.js
+++ src/frontend/pages/_app.js
@@ -1,9 +1,15 @@
+import Head from 'next/head';
 import Layout from '../components/layouts/layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>SARENKA</title>
+        <meta name="description" content=""/>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      </Head>
       <div className="container">
       <Component {...pageProps} /> 
       </div>
