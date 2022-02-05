961254dd2e69dca65e957f762c7609297a4da6c4
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-02-05 22:28:41 +0100

    docker postgresql

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
