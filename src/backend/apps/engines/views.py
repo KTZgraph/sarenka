bcca2f4e1eb5ae104831f79197e1469051e3b8ed
pawlaczykdominika <pawlaczyk@protonmail.com>
2022-05-04 21:35:35 +0200

    python first test

--- src/backend/apps/engines/views.py
+++ src/backend/apps/engines/views.py
@@ -1,5 +1,8 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("ENGINES index.")
+@api_view(http_method_names=["GET"])  # żeby jako post endpoint traktował
+def index(request) -> Response:
+    return Response({"status": "success", "info": "ENGINES index."}, status=200)
