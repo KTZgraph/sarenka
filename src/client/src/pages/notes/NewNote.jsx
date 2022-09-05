18773b39c53fc0275e3fb32721d4025248be3e57
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-09-06 00:10:55 +0200

    Quill TOOLBAR_OPTIONS

--- src/client/src/pages/notes/NewNote.jsx
+++ src/client/src/pages/notes/NewNote.jsx
@@ -5,6 +5,18 @@ import "quill/dist/quill.snow.css";
 
 import "./NewNote.scss";
 
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+];
+
 // npm i quill
 const NewNote = () => {
   const wrapperRef = useCallback((wrapper) => {
@@ -15,7 +27,7 @@ const NewNote = () => {
     const editor = document.createElement("div");
     wrapper.append(editor);
 
-    new Quill(editor, { theme: "snow" });
+    new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS } });
   }, []);
 
   return (
