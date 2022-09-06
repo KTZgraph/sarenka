9b208d57030243ab1280a97461d5c81aff2818de
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-09-06 19:22:20 +0200

    sidebar update

--- src/client/src/components/organisms/sidebar/Sidebar.jsx
+++ src/client/src/components/organisms/sidebar/Sidebar.jsx
@@ -1,5 +1,4 @@
 import DashboardRoundedIcon from "@mui/icons-material/DashboardRounded";
-import GppBadRoundedIcon from "@mui/icons-material/GppBadRounded";
 import AddModeratorRoundedIcon from "@mui/icons-material/AddModeratorRounded";
 import PolicyRoundedIcon from "@mui/icons-material/PolicyRounded";
 import WidgetsRoundedIcon from "@mui/icons-material/WidgetsRounded";
@@ -9,6 +8,11 @@ import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
 import InsertChartRoundedIcon from "@mui/icons-material/InsertChartRounded";
 import DocumentScannerRoundedIcon from "@mui/icons-material/DocumentScannerRounded";
 import ContactPageRoundedIcon from "@mui/icons-material/ContactPageRounded";
+import NoteAddIcon from "@mui/icons-material/NoteAdd";
+import ReportIcon from "@mui/icons-material/Report";
+import GppMaybeIcon from "@mui/icons-material/GppMaybe";
+import SettingsInputHdmiIcon from "@mui/icons-material/SettingsInputHdmi";
+import AddBoxIcon from "@mui/icons-material/AddBox";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { DarkModeContext } from "../../../context/darkModeContext";
@@ -34,10 +38,10 @@ const Sidebar = () => {
               <span>home</span>
             </li>
           </Link>
-          <p className="title">main</p>
+          <p className="title">vulnerabilities</p>
           <Link to="/vulns/cves">
             <li>
-              <GppBadRoundedIcon className="icon" />
+              <GppMaybeIcon className="icon" />
               <span>vulnerabilities</span>
             </li>
           </Link>
@@ -53,10 +57,30 @@ const Sidebar = () => {
               <span>search</span>
             </li>
           </Link>
+          <p className="title">software</p>
           <Link to="/">
             <li>
               <WidgetsRoundedIcon className="icon" />
               <span>software</span>
+            </li>
+          </Link>
+          <Link to="/">
+            <li>
+              <ReportIcon className="icon" />
+              <span>End-of-Life</span>
+            </li>
+          </Link>
+          <p className="title">detectors</p>
+          <Link to="/">
+            <li>
+              <SettingsInputHdmiIcon className="icon" />
+              <span>detector</span>
+            </li>
+          </Link>
+          <Link to="/">
+            <li>
+              <AddBoxIcon className="icon" />
+              <span>add detector</span>
             </li>
           </Link>
           <p className="title">settings</p>
@@ -96,6 +120,12 @@ const Sidebar = () => {
             <li>
               <ContactPageRoundedIcon className="icon" />
               <span>my notes</span>
+            </li>
+          </Link>
+          <Link to="/notes/new">
+            <li>
+              <NoteAddIcon className="icon" />
+              <span>add note</span>
             </li>
           </Link>
         </ul>
