a319bf86a5d23b68d478cdbca671c5051d254ce3
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-09-07 00:28:38 +0200

    33:33 https://www.youtube.com/watch?v=rxRYEXBmM88

--- src/service-core/users/views.py
+++ src/service-core/users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+# https://www.django-rest-framework.org/api-guide/status-codes/
+from rest_framework import permissions, status
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        data = request.data
+
+        return Response({}, status=status.HTTP_201_CREATED)
+
+
+class RetrieveUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        pass
