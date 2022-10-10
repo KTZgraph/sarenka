64917b275ef01454501696ca8d52dd0edb99590e
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-10-10 23:03:22 +0200

    8:29 kształt krajów

--- src/client/src/core/visualizations/components/world-map/GeoChart.jsx
+++ src/client/src/core/visualizations/components/world-map/GeoChart.jsx
@@ -1,5 +1,16 @@
+/*
+https://www.youtube.com/watch?v=gGORNzKIXL4&list=PLDZ4p-ENjbiPo4WH7KdHjh_EMI7Ic8b2B&index=13
+kule ziemi trzeba zamienić na piskelowe wartości mapy
+https://github.com/d3/d3-geo#azimuthal-projections
+
+geoPath z d3.js - zwraca funkcję która otrzymuje jakiś geo.json data or features in a GEO JSON file or countires in our case as an input
+and transform them into "d" atributes of a <path> element in ans SVG
+- co chcemy zrobić to wyrenderować <path> element dla każdego kraju w świecie i połaczyć te atrybuty  "d" żeby zdefiniowac ich kształt
+
+*/
+
 import { useEffect, useRef } from 'react';
-import { select } from 'd3';
+import { select, geoPath, geoMercator } from 'd3';
 import useResizeObserver from '../../../../hooks/useResizeObserver';
 
 /*
@@ -17,8 +28,29 @@ const GeoChart = ({ data, property }) => {
 
     // use resized dimensions
     // but fall back to getBoundingClientReact, if no dimension yet
+    //   WARNING optymalizacja - bo diemsnions na początku jest nullem
     const { widt, height } =
       dimensions || wrapperRef.current.getBoundingClientRect();
+
+    //   WARNING projects geo-coordinates on a 2D plane
+    const projection = geoMercator();
+
+    //WARNING   takes geojson data
+    //WARNING   transform that into the d attribute of a path element
+    //   pathGenerator tę funckje zwrca geoPath, funckaj użyta później do zamaiany jsona na kształty
+    const pathGenerator = geoPath().projection(projection);
+
+    //   WARNING general update pattern żeby wygenerować kształy krajów
+    //    data.features z pliku jsona wszsytkie kraje ze światra
+    svg
+      .selectAll('.country')
+      .data(data.features)
+      //   na podstawie danych ma towrzyc elementy <path> svg
+      .join('path')
+      .attr('class', 'country')
+      //   definiowanie kształtów krajów - atrybut "d" elementu <path> svg
+      //   pathGenerator zwrócić prawidłowy atrybut d = "M100 100" i tak dalej
+      .attr('d', (feature) => pathGenerator(feature));
   }, [data, dimensions, property]);
 
   return (
