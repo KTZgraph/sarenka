1297e3692696213a2e753509c872000f9cf92c97
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-10-11 16:27:02 +0200

    dodanie osi X

--- src/client/src/core/visualizations/components/stacked-bar-chart/StackedBarChart.jsx
+++ src/client/src/core/visualizations/components/stacked-bar-chart/StackedBarChart.jsx
@@ -1,4 +1,4 @@
-import { select } from "d3";
+import { select, scaleBand, axisBottom } from "d3";
 import { useEffect, useRef } from "react";
 import useResizeObserver from "../../../../hooks/useResizeObserver";
 
@@ -11,12 +11,26 @@ const StackedBarChart = ({ data, keys, colors }) => {
     const svg = select(svgRef.current);
     const { width, height } =
       dimensions || wrapperRef.current.getBoundingClientRect();
+
+    //   sklaa X jest potrzebna do xAxis
+    const xScale = scaleBand()
+      // domain input to lata, map zwraca listę
+      .domain(data.map((d) => d.year))
+      .range([0, width]);
+
+    // renderuje oś X
+    const xAxis = axisBottom(xScale);
+    // teraz dodać do svg
+    svg.select(".x-axis").call(xAxis);
   }, [colors, data, dimensions, keys]);
 
   return (
     <>
-      <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
-        <svg ref={svgRef}>
+      <div
+        ref={wrapperRef}
+        style={{ marginBottom: "2rem", overflow: "visible" }}
+      >
+        <svg ref={svgRef} style={{ overflow: "visible" }}>
           <g className="x-axis" />
           <g className="y-axis" />
         </svg>
