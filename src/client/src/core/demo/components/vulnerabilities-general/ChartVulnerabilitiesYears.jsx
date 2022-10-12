8f3c3aabdcb226c9b74b7005dc49a94cd39f9aaf
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-10-12 21:53:37 +0200

    BUG drawChart(...) duplikuje na sobie osie

--- src/client/src/core/demo/components/vulnerabilities-general/ChartVulnerabilitiesYears.jsx
+++ src/client/src/core/demo/components/vulnerabilities-general/ChartVulnerabilitiesYears.jsx
@@ -7,6 +7,7 @@ import Dropdown from "../../../../UI/Dropdown";
 import useResizeObserver from "../../../../hooks/useResizeObserver";
 
 import D3ChartVulnerabilitiesYears from "./D3ChartVulnerabilitiesYears";
+import { drawChart } from "./D3ChartVulnerabilitiesYears";
 
 const DUMMY_DATA = {
   2022: [
@@ -34,6 +35,34 @@ const DUMMY_DATA = {
     { month: 11, critical: 46, high: 115, medium: 122, low: 309 },
     { month: 12, critical: 28, high: 149, medium: 246, low: 484 },
   ],
+  2020: [
+    { month: 1, critical: 32, high: 129, medium: 228, low: 304 },
+    { month: 2, critical: 74, high: 163, medium: 259, low: 192 },
+    { month: 3, critical: 29, high: 197, medium: 170, low: 395 },
+    { month: 4, critical: 100, high: 107, medium: 273, low: 316 },
+    { month: 5, critical: 22, high: 100, medium: 273, low: 485 },
+    { month: 6, critical: 53, high: 118, medium: 183, low: 485 },
+    { month: 7, critical: 53, high: 178, medium: 274, low: 269 },
+    { month: 8, critical: 50, high: 193, medium: 192, low: 252 },
+    { month: 9, critical: 89, high: 116, medium: 244, low: 199 },
+    { month: 10, critical: 80, high: 125, medium: 190, low: 393 },
+    { month: 11, critical: 46, high: 115, medium: 122, low: 309 },
+    { month: 12, critical: 28, high: 149, medium: 246, low: 484 },
+  ],
+  2019: [
+    { month: 1, critical: 32, high: 129, medium: 228, low: 304 },
+    { month: 2, critical: 74, high: 163, medium: 259, low: 192 },
+    { month: 3, critical: 29, high: 197, medium: 170, low: 395 },
+    { month: 4, critical: 100, high: 107, medium: 273, low: 316 },
+    { month: 5, critical: 22, high: 100, medium: 273, low: 485 },
+    { month: 6, critical: 53, high: 118, medium: 183, low: 485 },
+    { month: 7, critical: 53, high: 178, medium: 274, low: 269 },
+    { month: 8, critical: 50, high: 193, medium: 192, low: 252 },
+    { month: 9, critical: 89, high: 116, medium: 244, low: 199 },
+    { month: 10, critical: 80, high: 125, medium: 190, low: 393 },
+    { month: 11, critical: 46, high: 115, medium: 122, low: 309 },
+    { month: 12, critical: 28, high: 149, medium: 246, low: 484 },
+  ],
 };
 
 const currentYear = new Date().getFullYear().toString();
@@ -47,6 +76,7 @@ const yearOptions = Object.keys(DUMMY_DATA).map((k) => ({
 const ChartVulnerabilitiesYears = () => {
   const wrapperRef = useRef();
   const svgRef = useRef();
+  //   BUG z dimension - uwaga na CSS
   const dimensions = useResizeObserver(wrapperRef);
   const [chartState, setChartState] = useState(null);
 
@@ -56,24 +86,15 @@ const ChartVulnerabilitiesYears = () => {
 
   useEffect(() => {
     // *------------------------------------------------
-    const { width, height } =
-      dimensions || wrapperRef.current.getBoundingClientRect();
+    if (!dimensions) return;
+    console.log("dimensions: ", dimensions);
+    const { width, height } = dimensions;
 
-    setData(vulnerabitilitesYearsDummy[parseInt(yearSelected)]);
+    // const svg = select(svgRef.current)
+    //   .attr("width", width)
+    //   .attr("height", height);
 
-    if (!chartState) {
-      setChartState(
-        new D3ChartVulnerabilitiesYears(
-          svgRef.current,
-          data,
-          yearSelected,
-          width,
-          height
-        )
-      );
-    } else {
-      chartState.update(data, yearSelected, width, height);
-    }
+    drawChart(svgRef.current, data, yearSelected, width, height);
 
     // *------------------------------------------------
   }, [data, yearSelected, chartState, dimensions]);
@@ -86,7 +107,9 @@ const ChartVulnerabilitiesYears = () => {
           label="Select Year"
           options={yearOptions}
           value={yearSelected}
-          onChange={(e) => setYearSelected(e.target.value)}
+          onChange={(e) =>
+            setData(vulnerabitilitesYearsDummy[parseInt(e.target.value)])
+          }
         />
         <svg
           ref={svgRef}
--- src/client/src/core/demo/components/vulnerabilities-general/D3ChartVulnerabilitiesYears.js
+++ src/client/src/core/demo/components/vulnerabilities-general/D3ChartVulnerabilitiesYears.js
@@ -1,45 +1,43 @@
 import { select, scaleLinear, scaleBand, axisBottom, max, axisLeft } from "d3";
 const MARGIN = { TOP: 50, BOTTOM: 50, LEFT: 70, RIGHT: 50 };
 
-export default class D3ChartVulnerabilitiesYears {
-  constructor(svgElement, data, yearSelected, width, height) {
-    let vis = this;
-    // select("#vulnerabilities-year-chart-id").remove();    console.log("drawChart konstruktor");
-    vis.update(svgElement, data, yearSelected, width, height);
-  }
-  // svgRef.current, data, yearSelected, width, height
-  update(svgElement, data, yearSelected, width, height) {
-    const svg = select(svgElement)
-      //   .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("id", "vulnerabilities-year-chart-id");
+export function drawChart(svgElement, data, yearSelected, width, height) {
+  console.log("draw chart");
 
-    console.log("drawChart update");
+  const svg = select(svgElement)
+    //   .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("padding", "50px");
 
-    const vulnerabilitiesAll = data.map(
-      (d) => d.critical + d.high + d.medium + d.low
-    );
-    const vulnerabilitiesCritical = data.map((d) => d.critical);
-    const vulnerabilitiesHigh = data.map((d) => d.high);
-    const vulnerabilitiesMedium = data.map((d) => d.medium);
-    const vulnerabilitiesLow = data.map((d) => d.low);
+  console.log("data", data);
 
-    // WARNING  wykres słupkowy
-    const xScaleBar = scaleBand()
-      .domain(vulnerabilitiesAll.map((val, idx) => idx))
-      .range([0, width])
-      .padding(0.5);
+  const vulnerabilitiesAll = data.map(
+    (d) => d.critical + d.high + d.medium + d.low
+  );
+  const vulnerabilitiesCritical = data.map((d) => d.critical);
+  const vulnerabilitiesHigh = data.map((d) => d.high);
+  const vulnerabilitiesMedium = data.map((d) => d.medium);
+  const vulnerabilitiesLow = data.map((d) => d.low);
 
-    const yScaleBar = scaleLinear().domain([0, height]).range([height, 0]);
+  // WARNING  wykres słupkowy
+  const xScaleBar = scaleBand()
+    .domain(vulnerabilitiesAll.map((val, idx) => idx))
+    .range([0, width])
+    .padding(0.5);
 
-    //   osie
-    const xAxis = axisBottom(xScaleBar).ticks(vulnerabilitiesAll.length);
-    const yAxis = axisLeft(yScaleBar);
+  const yScaleBar = scaleLinear().domain([0, height]).range([height, 0]);
 
-    //   dodanie osi X do wykresu
-    svg.append("g").call(xAxis).attr("transform", `translate(0, ${height})`);
-    //   dodanie osi Y
-    svg.append("g").call(yAxis);
-  }
+  //   osie
+  const xAxis = axisBottom(xScaleBar).ticks(vulnerabilitiesAll.length);
+  const yAxis = axisLeft(yScaleBar);
+
+  //   dodanie osi X do wykresu
+  svg
+    .append("g")
+    .call(xAxis)
+    .attr("transform", `translate(0, ${height})`)
+    .attr("class", "x-axis");
+  //   dodanie osi Y
+  svg.append("g").call(yAxis).attr("class", "y-axis");
 }
