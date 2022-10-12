e9e63eb58e5b4e5f22034384990a5557d495701d
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-10-12 18:42:25 +0200

    wybór roku z obiektu któy ma listy

--- src/client/src/core/demo/components/vulnerabilities-general/ChartVulnerabilitiesYears.jsx
+++ src/client/src/core/demo/components/vulnerabilities-general/ChartVulnerabilitiesYears.jsx
@@ -2,16 +2,10 @@ import { useEffect, useRef, useState } from "react";
 import { vulnerabitilitesYearsDummy } from "./vulnerabilities-years-dummy";
 import Dropdown from "../../../../UI/Dropdown";
 
-const yearOptions = [
-  { label: "2022", value: 2022 },
-  { label: "2021", value: 2021 },
-  { label: "2020", value: 2020 },
-  { label: "2019", value: 2019 },
-  { label: "2018", value: 2018 },
-  { label: "2017", value: 2017 },
-  { label: "2016", value: 2016 },
-  { label: "2015", value: 2015 },
-];
+const yearOptions = Object.keys(vulnerabitilitesYearsDummy).map((k) => ({
+  label: k.toString(),
+  value: k,
+}));
 
 const ChartVulnerabilitiesYears = () => {
   const svgRef = useRef(null);
