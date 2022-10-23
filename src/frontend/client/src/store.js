b6c9015b75a159c29f9145f72d0d83073ce80fca
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-10-24 01:03:26 +0200

    49:57 https://www.youtube.com/watch?v=DGmX1FDdLZE

--- src/frontend/client/src/store.js
+++ src/frontend/client/src/store.js
@@ -1,22 +1,18 @@
 // https://youtu.be/DGmX1FDdLZE?t=2211
 // https://redux-toolkit.js.org/api/configureStore
 import { configureStore } from "@reduxjs/toolkit";
+// mój reducer
+import userReducer from "./features/user";
 
 export const store = configureStore({
-  reducer: {},
-});
-
-const initialState = {
-  first: {
-    second: {
-      third: {
-        valOne: "",
-        valTwo: "",
-        isAuthenticated: false,
-      },
-      fifth: "asdf",
-    },
-    fourth: "asdf",
+  reducer: {
+    /* kiedy chcę dostać się do wartości, które są zwiażane z userem, będe robić user.atrybutKtoryChce
+    trzeba będize użyć hooka z react-redux
+     import {useSelector} from 'react-redux'
+     const user = useSelector(state => state.user)
+    //  albo destrukturyzacja
+    const {isAuthenticated} = useSelector(state => state.user)
+     */
+    user: userReducer,
   },
-  sixth: "asfasdf",
-};
+});
