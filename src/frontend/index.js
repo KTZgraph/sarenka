315bb93ac6e847dc4bb090aea9bc44a82e25090c
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-10-23 21:53:25 +0200

    https://youtu.be/DGmX1FDdLZE?t=492

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -2,8 +2,12 @@
 // w folderze sarenka\src\frontend>
 // npm install --save express
 // npm install --save path
-
 // node index.js
+
+// żeby dynamicznie widac było zmiany
+// npm install --save nodemon
+// zmodyfikowac plik sarenka\src\frontend\package.json
+// npm start
 
 // bo clinet ma folder produkcyjny build i portrzebujemy serwrera expressa do serwisowania wlaśnie tego produkcyjnego folderu
 
@@ -13,13 +17,16 @@ const path = require("path");
 
 const app = express();
 
+// dodanie plików statycznych
+app.use(express.static("client/build"));
+
 // raoute do serwisowania tego folderu build
 app.get("*", (req, res) => {
   // sarenka\src\frontend\client\build\index.html, żeby nie wspisywać ściezki bezwzglednej
   const myPath = path.resolve(__dirname, "client", "build", "index.html");
   //WARNING   __dirname specjalna zmeinna Node.js do aktualnego folderu
-  console.log("__dirname", __dirname);
-  console.log("my path ", myPath);
+  //   console.log("__dirname", __dirname);
+  //   console.log("my path ", myPath);
   return res.sendFile(myPath);
 });
 
