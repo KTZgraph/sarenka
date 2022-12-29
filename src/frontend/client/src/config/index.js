37809e17ea4f8808fb99074010f1f899d09ef696
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-12-29 19:23:48 +0100

    new react typescript frontend

--- src/frontend/client/src/config/index.js
+++
@@ -1,8 +0,0 @@
-/*
-https://youtu.be/DGmX1FDdLZE?t=791
-So this way whenever I wanna utilize tha localhost:5000 API_URL I don't have to do this process.env.REACT_APP_API_URL;
-instead I just import this API_URL and just use this shorter naming, rather than this long naming here process.env.REACT_APP_API_URL;
-This is all about this config folder is for
-*/
-
-export const API_URL = process.env.REACT_APP_API_URL;
